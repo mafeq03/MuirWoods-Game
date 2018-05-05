05ac5f835717dd0645e55e503a79f190699fc4d0
mafeq03 <mafeq03@gmail.com>
2018-05-05 16:20:15 -0400

    Skeleton of Project

---
+++ starter-code/redwoods.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+
+ 
+  }
+
+var canvas = document.getElementById('redwoods');
+var ctx = canvas.getContext('2d');
+
+//Characters
+
+//creates game
+
+
+
+//Good plants
+let madrone = {
+
+}
+
+let caliHazelnut ={
+
+}
+
+let 
+//Poisonous Plants
+
+//
